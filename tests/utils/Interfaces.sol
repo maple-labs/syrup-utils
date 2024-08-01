2cb504590430d9a90ba7be96a8f6476def926c2c
Vedran Bidin <vedran@bidin.me>
2024-08-01 12:28:23 +0300

    test: Add scenario tests for `SyrupUserActions` (#41)
    * feat: add scenario tests for syrup user actions
    
    * fix: add psm low liquidity scenario
    
    * test: add impairment scenario
    
    * fix: add address to params
    
    * fix: test_scenario_slippage test
    
    * fix: tests
    
    * test: add fuzzing scenario test (#53)
    
    Signed-off-by: Vedran Bidin <vedran@bidin.me>
    
    * fix: update comments
    
    ---------
    
    Signed-off-by: Vedran Bidin <vedran@bidin.me>
    Co-authored-by: Farhaan <59924029+0xfarhaan@users.noreply.github.com>

--- tests/utils/Interfaces.sol
+++ tests/utils/Interfaces.sol
@@ -1,6 +1,64 @@
 // SPDX-License-Identifier: BUSL-1.1
 pragma solidity ^0.8.0;
 
+interface IBalancerVaultLike {
+
+    enum SwapKind { GIVEN_IN, GIVEN_OUT }
+
+    struct FundManagement {
+        address sender;
+        bool fromInternalBalance;
+        address recipient;
+        bool toInternalBalance;
+    }
+
+    struct SingleSwap {
+        bytes32 poolId;
+        SwapKind kind;
+        address assetIn;
+        address assetOut;
+        uint256 amount;
+        bytes userData;
+    }
+
+    function swap(
+        SingleSwap calldata singleSwap,
+        FundManagement calldata funds,
+        uint256 limit,
+        uint256 deadline
+    ) external returns (uint256 assetDelta);
+
+    function getPoolTokenInfo(
+        bytes32 poolId,
+        address token
+    )
+        external
+        returns (
+            uint256 cash,
+            uint256 managed,
+            uint256 blockNumber,
+            address assetManager
+        );
+}
+
+interface IPotLike {
+
+    function drip() external;
+
+}
+
+interface ISDaiLike {
+
+    function deposit(uint256 assets, address receiver) external returns (uint256 shares);
+
+    function pot() external returns (address pot);
+
+    function previewRedeem(uint256 shares) external returns (uint256 assets);
+
+    function redeem(uint256 shares, address receiver, address owner) external returns (uint256 assets);
+
+}
+
 interface IStakedSyrupLike {
 
     function balanceOf(address account) external view returns (uint256);
